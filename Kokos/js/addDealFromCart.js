9c74a781e6085345d0b0f192e24209d6867f607c
Mevericks <reloved@gmail.com>
2020-07-17 18:25:24 +0300

    [POOHAN KOKOS] добавил addDealFromCart

---
+++ Kokos/js/addDealFromCart.js
@@ -0,0 +1,60 @@
+function get_products_from_cart(){
+    var data = [];
+
+    for (var index = 0; index < window.tcart.products.length; index++) {
+        data.push({name:"Продукт",value:window.tcart.products[index].name});
+        if(window.tcart.products[index].options != undefined){
+            for (var i = 0; i< window.tcart.products[index].options.length; i++) {
+                data.push({name:window.tcart.products[index].options[i].option,value:window.tcart.products[index].options[i].variant});
+            }
+        }
+    }
+    return data;
+}
+
+$(document).ready(function() {
+
+    var $mev_cart_deal_form = $('.t706__orderform form');
+
+    $mev_cart_deal_form.on('click', 'button', function(event) {
+        var arErrors = window.tildaForm.validate($mev_cart_deal_form);
+        if(arErrors.length>0){
+            // ничего не делаем, есть ошибки при заполнении формы
+        }else{
+            var form_data = $mev_cart_deal_form.serializeArray(),
+            details_data = get_products_from_cart();
+
+            form_data.push({
+                'name':'product_price',
+                'value':window.tcart.amount
+            });
+            form_data.push({
+                'name':'site_url',
+                'value':window.location.hostname+window.location.pathname
+            })
+
+            var data_to_send = {
+                form_data: form_data,
+                details_data: details_data
+            };
+
+            $.ajax({
+                url: 'https://todobox.ru/payment/kokoslook/kokos/bitrix24-sdk.php' + window.location.search,
+                type: 'post',
+                dataType: 'json',
+                data: data_to_send,
+            })
+            .done(function(data) {
+
+            })
+            .fail(function(data) {
+                // console.log("error");
+            })
+            .always(function() {
+                // console.log("complete");
+            });
+        }
+
+    });
+
+});
